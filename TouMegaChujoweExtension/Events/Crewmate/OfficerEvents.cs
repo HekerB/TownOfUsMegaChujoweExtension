3d0aeec0999e2411f8b70102e77c2a4498e9d3dd
Kajoja <Kosior223@gmail.com>
2026-05-12 23:00:01 +0200

    Update OfficerEvents.cs

--- TouMegaChujoweExtension/Events/Crewmate/OfficerEvents.cs
+++ TouMegaChujoweExtension/Events/Crewmate/OfficerEvents.cs
@@ -1,7 +1,11 @@
+using HarmonyLib;
 using MiraAPI.Events;
 using MiraAPI.Events.Vanilla.Gameplay;
+using MiraAPI.Hud;
+using TownOfUs.Buttons.Crewmate;
 using TownOfUs.Modules;
 using TownOfUs.Roles.Crewmate;
+using UnityEngine;
 
 namespace TouMegaChujoweExtension.Events.Crewmate;
 
@@ -14,6 +18,55 @@ public static class OfficerEvents
         if (source.Data.Role is OfficerRole && GameHistory.PlayerStats.TryGetValue(source.PlayerId, out var stats))
         {
             stats.CorrectKills += 1;
+        }
+    }
+
+    [RegisterEvent]
+    public static void RoundStartEventHandler(RoundStartEvent @event)
+    {
+        var localPlayer = PlayerControl.LocalPlayer;
+        if (localPlayer == null || localPlayer.Data.Role is not OfficerRole)
+        {
+            return;
+        }
+
+        var shootButton = CustomButtonSingleton<OfficerShootButton>.Instance;
+        var loadButton = CustomButtonSingleton<OfficerLoadButton>.Instance;
+
+        var initialCd = 10f;
+
+        if (shootButton != null)
+        {
+            shootButton.SetTimer(Mathf.Max(shootButton.Timer, initialCd));
+        }
+
+        if (loadButton != null)
+        {
+            loadButton.SetTimer(Mathf.Max(loadButton.Timer, initialCd));
+        }
+    }
+
+    [HarmonyPatch(typeof(OfficerRole), nameof(OfficerRole.OnMeetingStart))]
+    public static class OfficerMeetingStartPatch
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(OfficerRole __instance)
+        {
+            __instance.RoundsBeforeReset--;
+
+            if (__instance.Player.AmOwner)
+            {
+                if (__instance.RoundsBeforeReset > 0)
+                {
+                    OfficerRole.RpcOfficerSyncBullets(__instance.Player, __instance.RoundsBeforeReset, __instance.TotalBullets, 0);
+                }
+                else
+                {
+                    OfficerRole.RpcOfficerSyncBullets(__instance.Player, 0, __instance.TotalBullets, __instance.LoadedBullets);
+                }
+            }
+
+            return false;
         }
     }
 }
